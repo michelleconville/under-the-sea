9121c2dcf883f2f5f453687fc80c28947f6c68fd
michelleconville <michelle.conville@gmail.com>
2022-12-14 12:56:33 +0000

    Add flip card function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
-function flipcard() {
+const cards = document.querySelectorAll('.memory-card');
 
+function flipCard() {
+    this.classList.toggle('flip');
 }
+
+cards.forEach(card => card.addEventListener('click', flipCard)); 
+
+
+
+
+
 
 
 
